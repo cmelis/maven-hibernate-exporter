865165b7bde11c0c257e7e1ca2dc86089b6b8390
petr.cmela@gmail.com <petr.cmela@gmail.com>
2012-12-03 20:43:54 +0100

    Add packaging type and build element into pom.xml. Add annotation into PluginExec class.

--- src/main/java/maven/PluginExec.java
+++ src/main/java/maven/PluginExec.java
@@ -4,11 +4,12 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
+/**
+ * @goal helloworld
+ */
 public class PluginExec extends AbstractMojo{
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		getLog().info("Maven Plugin for Hibernate Export");
-		
 	}
-
 }
